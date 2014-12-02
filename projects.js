07f731d317c5a236870700e3e612df5b99ccdd07
YOUR NAME <erinpagemd@yahoo.com>
2014-12-02 01:35:57 -0600

    move stuff around

--- projects.js
+++ projects.js
@@ -2,6 +2,51 @@ if (Meteor.isClient) {
   Template.body.helpers({
     projects: [
       {
+        name: 'Catanator',
+        picture: 'img/catanator.png',
+        link: 'http://erinpagemd.github.io/catanator/',
+        repo: 'https://github.com/erinpagemd/catanator',
+        stack: 'phaser.io',
+        desc: 'First dive into games! Adaptation of a phaser.io tutorial.'
+      },
+
+      {
+        name: 'Appchiever',
+        picture: 'img/appchiever.png',
+        link: 'http://appchiever.meteor.com',
+        repo: 'https://github.com/erinpagemd/appchiever',
+        stack: 'meteor.js, javascript',
+        desc: 'Adaptation of meteor tutorial. High School students can build a list of their activities for college applications.'
+      },
+
+      {
+        name: 'NashViva',
+        picture: 'img/nashviva.png',
+        link: 'http://nashviva.codefornashville.org/',
+        repo: 'https://github.com/code-for-nashville/nashviva',
+        stack: 'css, html',
+        desc: 'Created a website at HackNashville6 using the open data portal of Metro Nashville. Made in collaboration with other members of the Nashville Brigade. Contributed to CSS and HTML.'
+      },
+
+      {
+        name: 'Retirement Age',
+        picture: 'img/retirementage.png',
+        link: 'http://erinpagemd.github.io/retirement_age/',
+        repo: 'https://github.com/erinpagemd/retirement_age',
+        stack: 'javascript, css, html, jquery',
+        desc: 'dynamically add text based on specific criteria'
+      },
+
+      {
+        name: 'Project Page',
+        picture: 'img/projects.png',
+        link: 'http://ep_projects.meteor.com/',
+        repo: 'https://github.com/erinpagemd/projects',
+        stack: 'meteor.js, javascript',
+        desc: 'Meteor project from scratch! Displaying coding projects.'
+      },
+
+      {
         name: 'Javascript Calculator',
         picture: 'img/javascriptCalculator.png',
         link: 'http://erinpagemd.github.io/JavascriptCalculator/',
@@ -29,15 +74,6 @@ if (Meteor.isClient) {
       },
 
       {
-        name: 'Retirement Age',
-        picture: 'img/retirementage.png',
-        link: 'http://erinpagemd.github.io/retirement_age/',
-        repo: 'https://github.com/erinpagemd/retirement_age',
-        stack: 'javascript, css, html, jquery',
-        desc: 'dynamically add text based on specific criteria'
-      },
-
-      {
         name: 'Palindrome',
         picture: 'img/palindrome.png',
         link: 'http://erinpagemd.github.io/Palindrome/',
@@ -56,48 +92,12 @@ if (Meteor.isClient) {
       },
 
       {
-        name: 'Appchiever',
-        picture: 'img/appchiever.png',
-        link: 'http://appchiever.meteor.com',
-        repo: 'https://github.com/erinpagemd/appchiever',
-        stack: 'meteor.js, javascript',
-        desc: 'Adaptation of meteor tutorial. High School students can build a list of their activities for college applications.'
-      },
-
-      {
-        name: 'NashViva',
-        picture: 'img/nashviva.png',
-        link: 'http://nashviva.codefornashville.org/',
-        repo: 'https://github.com/code-for-nashville/nashviva',
-        stack: 'css, html',
-        desc: 'Created a website at HackNashville6 using the open data portal of Metro Nashville. Made in collaboration with other members of the Nashville Brigade. Contributed to CSS and HTML.'
-      },
-
-      {
         name: 'Codefornashville.org',
         picture: 'img/codefornashville.png',
         link: 'http://www.codefornashville.org/',
         repo: 'https://github.com/code-for-nashville/code-for-nashville.github.io',
         stack: 'css, html, jekyll',
         desc: 'Collaborated on website for the Nashville Brigade. Created the contact form, currently only on dev branch.'
-      },
-
-      {
-        name: 'Catanator',
-        picture: 'img/catanator.png',
-        link: 'http://erinpagemd.github.io/catanator/',
-        repo: 'https://github.com/erinpagemd/catanator',
-        stack: 'phaser.io',
-        desc: 'First dive into games! Adaptation of a phaser.io tutorial.'
-      },
-
-      {
-        name: 'Project Page',
-        picture: 'img/projects.png',
-        link: 'http://ep_projects.meteor.com/',
-        repo: 'https://github.com/erinpagemd/projects',
-        stack: 'meteor.js, javascript',
-        desc: 'Meteor project from scratch! Displaying coding projects.'
       },
 
       {
@@ -110,7 +110,7 @@ if (Meteor.isClient) {
       },
 
       {
-        name: 'Bank',
+        name: 'BankJS',
         picture: 'img/catanator.png',
         link: 'http://erinpagemd.github.io/catanator/',
         repo: 'https://github.com/erinpagemd/catanator',
@@ -134,7 +134,7 @@ if (Meteor.isClient) {
         repo: 'https://github.com/erinpagemd/catanator',
         stack: 'phaser.io',
         desc: 'First dive into games! Adaptation of a phaser.io tutorial.'
-      }      
+      }
     ]
   });
 
